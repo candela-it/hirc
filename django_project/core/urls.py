23286ef7506fcd23c2714297819b051ab069ccb6
Dražen Odobašić <dodobas@candela-it.com>
2014-05-17 20:28:12 +0200

    Initial commit

---
+++ django_project/core/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns(
+    '',
+
+    # Enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+    # Examples:
+    # url(r'^hirc_app/', include('hirc_app.foo.urls')),
+)
