930c154dd1e2ea8c8f27491077d985833a3ec3e4
Zvonimir Lepur <zvonimir.lepur@gmail.com>
2014-05-18 11:35:18 +0200

    Added CustomUser.

--- django_project/imagery_requests/models.py
+++ django_project/imagery_requests/models.py
@@ -2,3 +2,9 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class CustomUser(User):
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=50)
