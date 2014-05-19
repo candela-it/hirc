bb8b2336ac160b1cb1e3903a747dc729d3322181
Igor Crnkovic <icrni@candela-it.com>
2014-05-19 11:40:03 +0200

    bing and map layers for map

---
+++ django_project/imagery_requests/static/js/map.js
@@ -0,0 +1,24 @@
+function initMap() {
+    map = L.map('map').setView([51.505, -0.09], 2);
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osmTiles = L.tileLayer(osmUrl, {attribution: osmAttrib});
+    // In the URL below, replace 'examples.map-zr0njcqy' with your map id.
+    var mapboxUrl = 'https://{s}.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png';
+    var mapboxAttrib = '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>';
+    var mapboxTiles = L.tileLayer(mapboxUrl, {attribution: mapboxAttrib});
+
+    var bingUrl = 'http://t{s}.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1398';
+    var bingAttrib = '&copy; <a href="http://bing.com/maps">Bing Maps</a>';
+    var bingTiles = new BingLayer(bingUrl, {subdomains: ['0', '1', '2', '3', '4'], attribution: bingAttrib});
+
+    map.addLayer(bingTiles);
+    $(document).ready(function(){
+      var overlays = {
+        'osm': osmTiles,
+        'mapbox': mapboxTiles,
+        'bing': bingTiles,
+      };
+      L.control.layers(overlays).addTo(map);
+    });
+}
