41cd27cb552fb4b48d81ff23cd5438ae20cdc575
Dražen Odobašić <dodobas@candela-it.com>
2014-05-20 18:21:08 +0200

    Add basic login required permission
    Issue #47

--- django_project/imagery_requests/views.py
+++ django_project/imagery_requests/views.py
@@ -5,20 +5,24 @@ from django.views.generic import ListView, UpdateView, DetailView, CreateView
 from django.views.generic.detail import BaseDetailView
 from django import http
 
+from braces.views import LoginRequiredMixin
+
 from .forms import ImageryRequestForm, ImageryRequestEditForm
 from .models import ImageryRequest
 
 
-class ListRequests(ListView):
+class ListRequests(LoginRequiredMixin, ListView):
     template_name = 'list_requests.html'
     context_object_name = 'requests'
     model = ImageryRequest
 
 
-class AddRequest(CreateView):
+class AddRequest(LoginRequiredMixin, CreateView):
     template_name = 'request_form.html'
     form_class = ImageryRequestForm
     model = ImageryRequest
+
+    raise_exception = True
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
@@ -27,20 +31,24 @@ class AddRequest(CreateView):
         return super(AddRequest, self).form_valid(form)
 
 
-class EditRequest(UpdateView):
+class EditRequest(LoginRequiredMixin, UpdateView):
     template_name = 'request_form.html'
     form_class = ImageryRequestEditForm
     model = ImageryRequest
 
+    raise_exception = True
 
-class ViewRequest(DetailView):
+
+class ViewRequest(LoginRequiredMixin, DetailView):
     context_object_name = 'request'
     model = ImageryRequest
     template_name = 'request_view.html'
 
 
-class DownloadRequest(BaseDetailView):
+class DownloadRequest(LoginRequiredMixin, BaseDetailView):
     model = ImageryRequest
+
+    raise_exception = True
 
     def render_to_response(self, context):
         request = context['object']
